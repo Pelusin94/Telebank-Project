0b843056232c2a2471da0b77f7b265ce51f57997
Gregory Vasquez <GVasquez@telebank-online.com>
2020-06-05 17:05:53 +0100

    05-06-2020w

--- fulfilment/views.py
+++ fulfilment/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from . import forms
+from . import models
+import csv
 
 
 def fulfilment_view(request):
@@ -8,11 +10,18 @@ def fulfilment_view(request):
     upload_form = forms.UploadFileForm()
     context = {'form': upload_form}
 
-    # if request.method == 'POST':
-    #
-    #     if request.POST.get('uploaddata'):
-    #         upload_form = forms.UploadFileForm(request.POST, request.FILES)
-    #         if upload_form.is_valid():
+    if request.method == 'POST':
+
+        if request.POST.get('uploaddata'):
+            upload_form = forms.UploadFileForm(request.POST, request.FILES)
+            if upload_form.is_valid():
+                model = models.FulfilmentFilesData.objects.all()
+                file = upload_form.cleaned_data['file_path']
+                data = file.read().decode('utf-8').splitlines()
+                dic_data = csv.DictReader(data)
+
+                for row in dic_data:
+                    print(row)
 
 
 
